67d9ab3594b999eee77e0d6d961df83d2f03377b
mkm1088 <35528858+mkm1088@users.noreply.github.com>
2019-02-06 20:38:30 +0800

    Add files via upload

---
+++ regression.R
@@ -0,0 +1,24 @@
+rawdata<-read.csv("F:/sem 2/DISBA/rita5/Folder_SLFiles (2)/ActualRatings_weeklyGRP.csv")
+pacman::p_load(tidyverse, lubridate,zoo,forecast, fUnitRoots)
+library(pacman)
+library(ggplot2)
+sapply(rawdata, class)
+str(rawdata)
+head(data,3)
+model=lm(GRP~as.numeric(GRPRatingsDate), data = rawdata)
+
+index <- sample(nrow(finalData), 0.7 * nrow(finalData))
+trainingData <- finalData[index, ]
+testingData <- finalData[-index, ]
+
+#rawdata$pred<-lm(GRP~., data = rawdata)
+summary(model)
+#rawdata$GRPRatingsDate<-as.numeric(rawdata$GRPRatingsDate)
+View(rawdata)
+rawdata$pred = (model$fitted.values)
+
+ggplot(testingData, aes(t))+ 
+geom_line(aes(y=GRP, colour="var0"))+
+geom_line(aes(y=pred_te, colour="var1"))
+res = residuals(model)
+plot(jitter(res)~jitter(GRP), data=rawdata, abline)
---
+++ time series.R
@@ -0,0 +1,32 @@
+rawdata<-read.csv("F:/sem 2/DISBA/rita5/Folder_SLFiles (2)/ActualRatings_weeklyGRP.csv")
+#grp = size of an advertising campaign , not reached audience
+#start(data)
+View(data)
+class(data)
+frequency(data)
+
+lm(GRP~GRPRatingsDate, data=rawdata)
+abline(reg=lm(rawdata~time(rawdata)))
+cycle(rawdata)
+plot(aggregate(rawdata,FUN=mean))
+
+#end(data)
+summary(rawdata)
+plot(rawdata)
+abline(reg=lm(data~time(data)))
+require(stats)
+reg<-lm(GRP ~ GRPRatingsDate, data = rawdata)
+coeff=coefficients(reg)
+eq = paste0("y = ", round(coeff[2],1))
+plot(rawdata, main=eq)
+abline(reg, col="blue")
+
+adf.test(diff(log(data)), alternative="stationary", k=0)
+data: diff(log(raw))
+
+
+
+
+
+GRP.lm<-lm(GRP~GRPRatingsDate, data=rawdata)
+summary(GRP.lm)
